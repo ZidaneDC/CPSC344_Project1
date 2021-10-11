26d0b9b4fe12e86ede93e8f2ff2142386c9d7d6b
micamarietta <micamarietta@gmail.com>
2021-10-11 11:31:07 -0700

    Basic Scene Layout Created
    - Main Screen/ main menu created
    - Story UI screen created
    - Main screen has a button to lead to story UI screen, story UI screen has a button to lead to level one
    - lighting settings adjusted for scene change
    - UI button event system toggled for button presses

---
+++ Assets/Scripts/SceneLoader.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneLoader : MonoBehaviour
+{
+    public void LoadStoryScene()
+    {
+        SceneManager.LoadScene("StoryScreen");
+    }
+
+    public void LoadLevelOne()
+    {
+        SceneManager.LoadScene("LevelOne");
+
+    }
+
+}
