17138129cd07557dbc37ff0fda544baab243a4aa
micamarietta <micamarietta@gmail.com>
2021-10-27 19:46:52 -0700

    Added Title Pop Ups after Dialog
    Title pop ups all over screen added after dialog is finished, showing game title. Audio for pop-ups is imported, but not connected to functionality

--- Assets/Scripts/DialogueManager.cs
+++ Assets/Scripts/DialogueManager.cs
@@ -14,11 +14,16 @@ public class DialogueManager : MonoBehaviour
 
     public float textSpeed;
 
+    //variable for array with listPopUps
+    public GameObject[] titlePopUps;
+
     //private bool buttonPress;
 
     public GameObject continueButton;
     public GameObject startButton;
     public GameObject loadLevelButton;
+
+    //public AudioClip Aclip;
 
     // Start is called before the first frame update
     void Start()
@@ -26,7 +31,13 @@ public class DialogueManager : MonoBehaviour
         sentences = new Queue<string>();
         continueButton.SetActive(false);
         loadLevelButton.SetActive(false);
-        
+
+        //looping through popUps to disable them on start
+        foreach (GameObject popUp in titlePopUps)
+        {
+            popUp.SetActive(false);
+        }
+
         //buttonPress = false;
     }
 
@@ -36,7 +47,7 @@ public class DialogueManager : MonoBehaviour
     //    {
     //        buttonPress = true;
     //        Debug.Log("space key down.");
-    //    }
+    //   }
     //}
 
     public void StartDialogue(Dialogue dialogue) //on start button press, the button will deactivate and the dialogue ui will apppear
@@ -106,9 +117,42 @@ public class DialogueManager : MonoBehaviour
     //    yield break;
     //}
 
+    IEnumerator loopThroughPopUps()
+    {
+        int timesRan = 0;
+        //AudioSource aud = GetComponent<AudioSource>(); 
+
+        //loop through popUps, and enable each one
+        foreach (GameObject popUps in titlePopUps)
+        {
+            //speed up the pop-Up after the first 5
+            if(timesRan < 4)
+            {
+                yield return new WaitForSeconds(1);
+            }
+            else if(timesRan < 7)
+            {
+                yield return new WaitForSeconds(0.5F);
+            }else if(timesRan < 10)
+            {
+                yield return new WaitForSeconds(0.3F);
+            }
+            else
+            {
+                yield return new WaitForSeconds(0.1F);
+            }
+
+            popUps.SetActive(true);
+            //aud.Play();
+            timesRan++;
+            
+        }
+    }
+
     void EndDialogue()
     {
         animator.SetBool("IsOpen", false);
         loadLevelButton.SetActive(true);
+        StartCoroutine(loopThroughPopUps());
     }
 }
