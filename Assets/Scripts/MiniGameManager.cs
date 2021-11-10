01b3d90f3f243fd5fcf5506d4b4b27e99b559aa9
micamarietta <micamarietta@gmail.com>
2021-11-09 17:43:19 -0800

    Added functionality to MinigameManager.cs + KeyPad Buttons
    Added Keypad buttons to Keypad UI in level one. Added logic in MinigameManager.cs that takes in user input using button clicks and compares it to the key that the user should enter. Currently, button click functionality isn't registering, so transferring the logic to key clicks instead.

--- Assets/Scripts/MiniGameManager.cs
+++ Assets/Scripts/MiniGameManager.cs
@@ -1,22 +1,38 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class MiniGameManager : MonoBehaviour
 {
     public UIManager uiManager;
+    public Text enteredCode;
+    public string desiredCode = "1234";
     
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        enteredCode.text = "";
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        //check if four digits have been entered (code is 4 digits)
+        if(enteredCode.text.Length == 4)
+        {
+            //code is correct, show player that the codse is valid/correct
+            if(enteredCode.text == desiredCode)
+            {
+                enteredCode.text = "VALID";
+            }
+            //else, code is entered incorrectly
+            else
+            {
+                enteredCode.text = "INVALID";
+            }
+        }
     }
 
     private void OnTriggerEnter(Collider other)
@@ -26,5 +42,11 @@ public class MiniGameManager : MonoBehaviour
             uiManager.ShowMinigamePopup();
 
         }
+    }
+
+    public void OnButtonTap(Text text)
+    {
+        //on button tap, concatenate a string to show the digit the user selected to tap
+        enteredCode.text = enteredCode.text + text;
     }
 }
